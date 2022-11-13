d93399bf049eb2aa7c9f31fa6f06341d3903c7e6
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-13 15:53:29 +0330

    add sidebar in home page and remove routes

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,19 +1,10 @@
-import { BrowserRouter, Routes } from "react-router-dom";
-import Favorite from "../favorites/favorites";
-import Feed from "../feed/feed";
-import Player from "../player/player";
-import Trending from "../trending/trending";
+import Sidebar from '../../components/sidebar/sidebar'
+import './home.css'
 
 export default function Home() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Routes path="/" element={<Home />} />
-                <Routes path="/feed" element={<Feed />} />
-                <Routes path="/trending" element={<Trending />} />
-                <Routes path="/player" element={<Player />} />
-                <Routes path="/favorites" element={<Favorite />} />
-            </Routes>
-        </BrowserRouter>
+        <div className="main-body">
+            <Sidebar />
+        </div>
     )
 }
