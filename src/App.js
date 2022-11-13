8bbf887678053c4a7f6378048ec825b63f864e86
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-13 15:52:51 +0330

    add routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Favorite from './screens/favorites/favorites';
+import Feed from './screens/feed/feed';
+import Home from './screens/home/home';
+import Library from './screens/library/library';
+import Player from './screens/player/player';
+import Trending from './screens/trending/trending';
 
 function App() {
   return (
-    <div className="App">
-    hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favorite' element={<Favorite />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/player' element={<Player />} />
+        <Route path='/trending' element={<Trending />} />
+        <Route path='/library' element={<Library />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
