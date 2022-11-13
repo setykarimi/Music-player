064387ee439f0580834e3d20bf1371e0ac2ba844
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-13 16:13:46 +0330

    add active className for NavLinks

--- src/components/sidebar/sidebarButton/sidebarButton.js
+++ src/components/sidebar/sidebarButton/sidebarButton.js
@@ -1,16 +1,21 @@
 import { IconContext } from 'react-icons';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './sidebarButton.css';
 
 export default function SidebarButton(props) {
+    const location = useLocation();
+    const isActive = location.pathname === props.to;
+    const btnClass = isActive ? "btn-body active": "btn-body";
     return (
         <NavLink to={props.to}>
-            <IconContext.Provider value={{size: '20px', className: "btn-icon"}}>
-            <div className='btn-body'>
-                {props.icon}
-                <p className='btn-title'>{props.title}</p>
+
+            <div className={btnClass}>
+                <IconContext.Provider value={{ size: '20px', className: "btn-icon" }}>
+                    {props.icon}
+                    <p className='btn-title'>{props.title}</p>
+                </IconContext.Provider>
             </div>
-            </IconContext.Provider>
+
         </NavLink>
     )
 }
