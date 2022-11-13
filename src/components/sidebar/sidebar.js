0e64f3eb4de23aa9bcf9a20217334a9dc2c3cb42
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-13 15:50:47 +0330

    create new folder for components, create sidebar component

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,21 @@
+import './sidebar.css';
+import SidebarButton from '../../../sidebarButton/sidebarButton';
+import { MdSpaceDashboard, MdFavorite } from 'react-icons/md';
+import { FaGripfire, FaPlay, FaSignOutAlt } from 'react-icons/fa';
+import { IoLibrary } from 'react-icons/io5'
+
+export default function Sidebar() {
+    return (
+        <div className='sidebar-container'>
+            <img src='' className='profile-img' alt='profile' />
+            <div>
+                <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard/>}/>
+                <SidebarButton title="Trending" to="/trending" icon={<FaGripfire/>}/>
+                <SidebarButton title="Player" to="/player" icon={<FaPlay/>}/>
+                <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite/>}/>
+                <SidebarButton title="Library" to="/" icon={<IoLibrary/>}/>
+            </div>
+            <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt/>}/>
+        </div>
+    )
+}
