ce6b748f98f6be0ff3655dd8fb92ff0f598cb13a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-14 15:09:52 +0330

    Set authentication check in useEffect, add Login and ternary operator for showing login  at first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './layout/layout';
+import Login from './screens/auth/login';
 import Favorite from './screens/favorites/favorites';
 import Feed from './screens/feed/feed';
 import Home from './screens/home/home';
 import Library from './screens/library/library';
 import Player from './screens/player/player';
 import Trending from './screens/trending/trending';
+import { setClientToken } from './spotify';
 
 function App() {
-  return (
+  const [token , setToken] = useState("");
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    const _token = hash.split("&")[0].split("=")[1];
+    if(!token && hash){
+    window.localStorage.setItem("token", _token);
+    setToken(_token);
+    setClientToken(_token);
+    }
+    else{
+      setToken(token);
+      setClientToken(_token)
+    }
+  },[])
+  return (!token ?
+    <Login /> : 
     <BrowserRouter>
     <Layout>
       <Routes>
