97e791fb5f46df5b8a5ef4df375490b8f139b6f8
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-15 14:49:18 +0330

    Add space after Baerer

--- src/spotify.js
+++ src/spotify.js
@@ -3,18 +3,18 @@ import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "442809f27ceb405fa44168c87978be58";
-const redirectUrl = "http://localhost:3000";
+const redirectUri = "http://localhost:3000";
 const scopes = ["user-library-read", "playlist-read-private"];
 
-export const LoginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUrl}&scopes=${scopes.join("20%")}&response_type=token&show_dialog=true`;
+export const LoginEndpoint = `${authEndpoint}response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scopes=${scopes.join("20%")}&response_type=token&show_dialog=true`;
 
 const apiClient = axios.create({
-    baseURL : "https://api/spotify.com/v1/",
+    baseURL: "https://api.spotify.com/v1/",
 })
 
 export const setClientToken = (token) => {
-    apiClient.interceptors.request.use(async function(config){
-        config.headers.Authorization = "Bearer" + token;
+    apiClient.interceptors.request.use(async function (config) {
+        config.headers.Authorization = "Bearer " + token;
         return config
     })
 }
