cf31d4739f27492a950e3ec6a975b75edf77226f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-15 14:53:07 +0330

    Add request method for getting user playlist and show it

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import apiClient from "../../spotify";
+import './library.css';
+
 export default function Library() {
-    return(
-        <div>Library</div>
+    const [playlists, setPlaylists] = useState([]);
+    useEffect(() => {
+        apiClient.get("me/playlists")
+            .then((res) => {
+                setPlaylists(res.data.items)
+                console.log(res.data.items)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <div className="library-body">
+            {playlists.map((playlist) => {
+                <div className="playlist-card">
+                        <img src={playlist.images[0].url} className="playlist-image" alt="playlist-art"/>
+                    <p>{playlist.name}</p>
+                    <p>{playlist.tracks.total} Songs</p>
+                </div>
+            })}
+        </div>
     )
-}
+} 
