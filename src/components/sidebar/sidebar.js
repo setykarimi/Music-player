2b27ea49134aad0345f69836e4b2117adf48a7b2
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-15 14:50:41 +0330

    Add name state for showing user name

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -7,22 +7,31 @@ import { useEffect, useState } from 'react';
 import apiClient from '../../spotify';
 
 export default function Sidebar() {
-    const [image, setImage] = useState("https://i1.wp.com/hypebeast.com/image/2020/07/apple-memoji-update-headwear-masks-hairstyles-7.png?w=1600")
-    useEffect(()=> {
+    const [image, setImage] = useState("https://i1.wp.com/hypebeast.com/image/2020/07/apple-memoji-update-headwear-masks-hairstyles-7.png?w=1600");
+
+    const [name,setName] = useState("")
+
+    useEffect(() => {
         apiClient.get("me")
-        .then(res => setImage(res.data.images[0].url))
-    },[])
+            .then(res => {
+                setImage(res.data.images[0].url)
+            setName(res.data.display_name)})
+            .catch(err => console.log(err))
+    }, [])
     return (
         <div className='sidebar-container'>
+            <div className='image-container'>
             <img src={image} className='profile-img' alt='profile' />
-            <div>
-                <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard/>}/>
-                <SidebarButton title="Trending" to="/trending" icon={<FaGripfire/>}/>
-                <SidebarButton title="Player" to="/player" icon={<FaPlay/>}/>
-                <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite/>}/>
-                <SidebarButton title="Library" to="/" icon={<IoLibrary/>}/>
+            <span>{name}</span>
             </div>
-            <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt/>}/>
+            <div>
+                <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+                <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+                <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+                <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />} />
+                <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
+            </div>
+            <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
         </div>
     )
 }
