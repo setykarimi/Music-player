8625baab9c03e1d2dca641a9eb0d2a005ca1af79
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-16 12:54:52 +0330

    add playlist fade in mapping playlist

--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { IconContext } from "react-icons";
+import { AiFillPlayCircle } from "react-icons/ai"
+import { useNavigate } from "react-router-dom";
 import apiClient from "../../spotify";
 import './library.css';
 
@@ -13,13 +16,24 @@ export default function Library() {
             .catch(err => console.log(err))
     }, [])
 
+    const navigate = useNavigate();
+
+    const playPlaylist = (id) => {
+        navigate('/player' , {state: { id: id}})
+    }
+
     return (
         <div className="library-body">
             {playlists.map((playlist) => {
-                <div className="playlist-card">
+                <div className="playlist-card" key={playlist.id} onClick={() => playPlaylist(playlist)}>
                         <img src={playlist.images[0].url} className="playlist-image" alt="playlist-art"/>
-                    <p>{playlist.name}</p>
-                    <p>{playlist.tracks.total} Songs</p>
+                    <p className="playlist-title">{playlist.name}</p>
+                    <p className="playlist-subtitle">{playlist.tracks.total} Songs</p>
+                    <div className="playlist-fade">
+                        <IconContext.Provider value={{size: "50px", color: "#e99D72"}}>
+                            <AiFillPlayCircle />
+                        </IconContext.Provider>
+                    </div>
                 </div>
             })}
         </div>
